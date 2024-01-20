c47978f967356dc27613751db72b109ce37b253c
yourusername <arevik.a2.y@tumo.org>
2024-01-20 13:59:47 +0400

    add node project

---
+++ public/script.js
@@ -0,0 +1,16 @@
+let text = document.getElementById("myText")
+console.log(text.innerText);
+
+let usName = document.getElementById("usName")
+let usAge = document.getElementById('usAge')
+
+function getVal() {
+    fetch("http://localhost:3000/addName",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({name: usName.value, age: usAge.value})
+    })
+  
+}
